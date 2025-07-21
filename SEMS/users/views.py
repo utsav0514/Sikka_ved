b1a0af59b4e1d9b5b16ea2ec971620c80c51c8b9
utsav <utshavshrestha.1713@gmail.com>
2025-07-21 20:35:31 +0545

    Setting up the SEMS

---
+++ SEMS/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import Usercreate  # Import your custom form
+
+def register(request):
+    if request.method == 'POST':
+        form = Usercreate(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('income_setup')  
+    else:
+        form = Usercreate()
+    return render(request, 'users/register.html', {'form': form})
